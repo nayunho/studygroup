d762065ebf5ae24cb7f3dcbd65ae1c733d873951
nayunho <ring147@naver.com>
2024-02-24 14:40:58 +0900

    problem1

--- 2.Hyerin/problem1/Main.java
+++ 2.Hyerin/problem1/Main.java
@@ -21,15 +21,14 @@ public class Main {
     	String[] words = {"leaf", "banana", "orange", "strawberry", "pear", "cat"};
         List<String> wordsList = new ArrayList<>();
 
-        for (int i = 0; i <= words.length; i++) {
+        for (int i = 0; i < words.length; i++) {
             wordsList.add(words[i]);
         }
 
         //메서드 호출 및 출력문
-        List<String> filteredWords = Main.getFilteredWords(wordsList);
+        List<String> filteredWords = getFilteredWords(wordsList);
 		
 	 System.out.println("5글자 이상인 단어들의 리스트:");
-
 	//메서드로 필터링한 글자들을 for문을 통해 프린트
 	 for (String word : filteredWords) {
 	       System.out.println(word);
@@ -41,16 +40,13 @@ public class Main {
         
 	List<String> filteredWords = new ArrayList<>();
         //1. 여기에 5글자 이상인 단어들만 filteredWords 리스트에 추가하도록 구현()
-        
-
-
-
-
-
-
-
-
+		for(int i=0;i<wordsList.size();i++) {
+			if(wordsList.get(i).length()>=5) {
+				filteredWords.add(wordsList.get(i));
+			}
+		}
+		 
+		return filteredWords;
   
-        return null;
     }
 }
