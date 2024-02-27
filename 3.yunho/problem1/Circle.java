72906939cc194c39cf1cb176da40b18763e96732
nayunho <90189209+nayunho@users.noreply.github.com>
2024-02-27 09:30:17 +0900

    Update Circle.java

--- 3.yunho/problem1/Circle.java
+++ 3.yunho/problem1/Circle.java
@@ -3,22 +3,14 @@ package test;
 public class Circle implements Shape {
 
 	// 접근 제어자 private를 활용한 변수선언
-	private double radius;
+
 	
 	//생성자
-	public Circle(double radius) {
-		this.radius=radius;
-	}
+
 	
 	//인터페이스 구현
-	@Override
-	public double area() {
-		return Math.PI*radius*radius;
-	}
 	
 	//일반메서드
-	public double perimeter() {
-		return 2*Math.PI*radius;
-	}
+
 }
 
